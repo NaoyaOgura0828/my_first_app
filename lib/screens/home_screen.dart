ea74249ba10216e155ab1b6397553a9b75c0961f
SyatyoAdmiral <naoya_ogura@icloud.com>
2021-06-27 00:47:24 +0900

    Containerの適用まで

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -13,10 +14,25 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text('はじめてのアプリ'),
         centerTitle: true,
       ),
-      body: Column(
-        children: <Widget>[
-          Text('あなたの名前を教えてください。')
-        ],
+      body: Container(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            Center(
+              child: Text('あなたの名前を教えて下さい。', style: TextStyle(fontSize: 10.0),),
+            ),
+            TextField(
+              keyboardType: TextInputType.text,
+              style: TextStyle(fontSize: 20.0),
+              controller: null, //TODO 入力した文字を管理するTextEditionContorollerの設定
+            ),
+            Image.asset('assets/images/hello.png'),
+            ElevatedButton(
+                onPressed: null, //TODO ボタンのクリック処理
+                child: Text('おしてね', style: TextStyle(fontSize: 18.0),),
+            )
+          ],
+        ),
       ),
     );
   }
