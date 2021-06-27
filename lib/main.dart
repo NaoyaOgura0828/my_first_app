fb508e8c14eee7e52f9d6317d4755690002caa7c
SyatyoAdmiral <naoya_ogura@icloud.com>
2021-06-27 15:48:58 +0900

    theme: ThemeData.dark(), を適用

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'はじめてのアプリ',
+      theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
